f62b96a8d1b5167313d5482cd24afadd1309f00c
Sumair198 <sumairk198@gmail.com>
2022-09-24 23:54:08 +0500

    hookpractice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,121 @@
-import logo from './logo.svg';
-import './App.css';
+//practice-1 Increment counter
+// import React, { useState } from "react";
+// let App = () =>
+// {
+//   const [currValue , updateValue] = useState(0)
 
-function App() {
+// let ChangeText = () =>
+// {
+//   var count =  currValue + 1
+//   updateValue(count)
+//   // console.log('Clicked')
+// }
+
+
+//   return(
+//     <>
+//     <h1>{currValue}</h1>
+//     <button onClick={ChangeText}>Click Me</button>
+//     </>
+//   )
+// }
+// export default App;
+
+//Practice-2 text change
+// import React, { useState } from "react";
+// let App = () =>
+// {
+//   const [currValue , updateValue] = useState('How are u !')
+
+// let ChangeText = () =>
+// {
+//   var text = 'I am fine'
+//   updateValue(text)
+//   // console.log('Clicked')
+// }
+
+
+//   return(
+//     <>
+//     <h1>{currValue}</h1>
+//     <button onClick={ChangeText}>Click Me</button>
+//     </>
+//   )
+// }
+// export default App;
+
+
+//Practice-3 toogle-text simple
+// import React, { useState } from "react";
+// let App = () =>
+// {
+//   const [currValue , updateValue] = useState('How are u !')
+
+// let ChangeText = () =>
+// { 
+//   // var text1 = 'How are u !'
+//   if(currValue == 'How are u !'){
+//     updateValue('I am fine')
+//   }
+//   else
+//   {
+//     updateValue('How are u !')
+//   }
+// }
+//   return(
+//     <>
+//     <h1>{currValue}</h1>
+//     <button onClick={ChangeText}>Click Me</button>
+//     </>
+//   )
+// }
+// export default App;
+
+//Practice-4 ArrayOfObject using useState and clearData
+// import React, { useState } from "react";
+// let App = () => {
+
+//   const Array1 = [
+//     {
+//       id: 0, name: 'sumair', age: 24
+//     }
+//     , {
+//       id: 0, name: 'uzair', age: 25
+//     }
+//     , {
+//       id: 0, name: 'umair', age: 28
+//     }
+//   ]
+//   const [currValue, setValue] = useState(Array1)
+//   let clearData = () => {
+//     setValue([])
+//   }
+//   return (
+//     <>
+//       {
+//         currValue.map((data) =>  <h1>Name : {data.name}  & Age : {data.age}</h1>)
+
+//       }
+//       <button onClick={clearData}>Clear</button>
+//     </>
+//   )
+// }
+// export default App;
+
+import React, {  useState } from "react";
+const App = () => {
+
+  const [currObject , setObject] = useState({ Name: 'Sumair', Age: 24, Job: 'Web Developer' })
+
+  let ChangeData = () =>
+  {
+    setObject({...currObject , Age : 45})
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>Name : {currObject.Name} & Age : {currObject.Age} & Job : {currObject.Job}</h1>
+      <button onClick={ChangeData}>Update</button>
+    </>
+  )
 }
-
 export default App;
